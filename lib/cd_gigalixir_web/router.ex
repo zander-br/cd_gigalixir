e6b883798ba4636d8fba7548ae67e9f45c33a1cd
Anderson Santos <51419725+zander-br@users.noreply.github.com>
2022-03-12 15:46:26 -0300

    feat/create pipeline
    * refactor: format config/prod
    
    * feat: add formater pipeline
    
    * chore: add credo and credo config
    
    * refactor: adjust conform credo
    
    * feat: add lint pipeline
    
    * chore: add coveralls.json with ignore files
    
    * feat: add coverage pipeline
    
    * chore: add sobelow and config
    
    * feat: add security pipeline
    
    * chore: add dialyxir
    
    * feat: add dialyzer pipeline
    
    * fix: pipeline lint and formater
    
    * fix: pipeline lint

--- lib/cd_gigalixir_web/router.ex
+++ lib/cd_gigalixir_web/router.ex
@@ -10,9 +10,9 @@ defmodule CdGigalixirWeb.Router do
     plug :put_secure_browser_headers
   end
 
-  pipeline :api do
-    plug :accepts, ["json"]
-  end
+  # pipeline :api do
+  #   plug :accepts, ["json"]
+  # end
 
   scope "/", CdGigalixirWeb do
     pipe_through :browser
@@ -32,6 +32,7 @@ defmodule CdGigalixirWeb.Router do
   # If your application does not have an admins-only section yet,
   # you can use Plug.BasicAuth to set up some basic authentication
   # as long as you are also using SSL (which you should anyway).
+  # coveralls-ignore-start
   if Mix.env() in [:dev, :test] do
     import Phoenix.LiveDashboard.Router
 
@@ -41,6 +42,8 @@ defmodule CdGigalixirWeb.Router do
       live_dashboard "/dashboard", metrics: CdGigalixirWeb.Telemetry
     end
   end
+
+  # coveralls-ignore-stop
 
   # Enables the Swoosh mailbox preview in development.
   #
